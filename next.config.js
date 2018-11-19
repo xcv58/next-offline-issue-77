7b4643573a394aa901158967f2d7d933ee341a44
Yihong <chenyihonglove@gmail.com>
2018-11-18 20:39:32 -0800

    Fix video on iOS devices (#1)
    - Add `sw.js` file which includes the rangeRequests for mp4 requests
    - Use `importScripts` to include `sw.js` file in generated SW file
    - Update server to handle `/sw.js` request

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const withOffline = require('next-offline')
 
-module.exports = withOffline()
+module.exports = withOffline({
+  workboxOpts: {
+    importScripts: ['./sw.js'],
+    runtimeCaching: [{ urlPattern: /^https?.*/, handler: 'networkFirst' }]
+  },
+  webpack: (config) => {
+    // this will output your push listener file to .next folder
+    // check CopyWebpackPlugin docs if you want to change the destination (e.g. /static or /.next/static)
+    config.plugins.push(new CopyWebpackPlugin(['./sw.js']));
+    return config
+  }
+});
